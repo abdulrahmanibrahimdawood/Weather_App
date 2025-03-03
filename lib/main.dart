d53f5eb3b7ae3c404fbcb0ae1127442d6dd53302
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2025-03-03 14:48:15 +0200

    fix error

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather/cubits/get_weather_cubit/get_weather_cubit.dart';
 import 'package:weather/cubits/get_weather_cubit/get_weather_state.dart';
 import 'package:weather/views/home_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
